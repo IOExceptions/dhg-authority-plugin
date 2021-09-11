708113b63f0d262533e440d604272238b0355662
GitHub Action <action@github.com>
2021-09-11 09:34:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ioexceptions/dhgauthorityplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ioexceptions.dhgauthorityplugin.services
+
+import com.github.ioexceptions.dhgauthorityplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
